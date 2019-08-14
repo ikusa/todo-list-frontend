7bb50d87a142510f0e4e85d8d256b3c5c9b5fb0d
Joshua <joshuasetiawan04@gmail.com>
2019-08-14 13:39:19 +0700

    add create todo query

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,14 @@
 import ApolloClient from 'apollo-boost';
+import {AsyncStorage} from 'react-native';
 
 export let client = new ApolloClient({
   uri: 'http://localhost:4000',
+  request: async (operation) => {
+    const token = await AsyncStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        token,
+      },
+    });
+  },
 });
