cdd3a9999ab091ad7f10e087d9f072c8e902c450
Joshua <joshuasetiawan04@gmail.com>
2019-08-14 14:45:24 +0700

    integrate update query

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import {StyleSheet, View, Text, CheckBox} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, CheckBox, TextInput} from 'react-native';
 import DateInput from './DateInput';
+import {useMutation} from '@apollo/react-hooks';
+import {updateTodoQuery} from '../query/todo';
+import {
+  UpdateTodoData,
+  UpdateTodoDataVariables,
+} from '../generated/UpdateTodoData';
+import {textInputStyleOutline} from './styles/TextInputStyle';
+import useDebounce from '../hooks/useDebounce';
 
 type Props = {
   id: string;
@@ -8,12 +16,42 @@ type Props = {
   done: boolean;
   dueDate: string;
 };
-export default function TaskItem({task, done, dueDate}: Props) {
+export default function TaskItem({
+  id,
+  task: taskProp,
+  done: doneProp,
+  dueDate: dueDateProp,
+}: Props) {
+  let [updateTodo] = useMutation<UpdateTodoData, UpdateTodoDataVariables>(
+    updateTodoQuery,
+  );
+  let [task, setTask] = useState(taskProp);
+  let [done, setDone] = useState(doneProp);
+  let [dueDate, setDueDate] = useState(new Date(dueDateProp));
+  let debouncedTask = useDebounce(task, 200);
+  let debouncedDone = useDebounce(done, 200);
+  let debouncedDueDate = useDebounce(dueDate, 200);
+  useEffect(() => {
+    updateTodo({
+      variables: {
+        id,
+        task: debouncedTask,
+        done: debouncedDone,
+        dueDate: debouncedDueDate,
+      },
+    });
+  }, [debouncedTask, id, updateTodo, debouncedDone, debouncedDueDate]);
   return (
     <View style={styles.taskContainer}>
-      <Text>{task}</Text>
-      <DateInput date={new Date(dueDate)} onChange={() => {}} />
-      <CheckBox value={done} />
+      <TextInput
+        style={textInputStyleOutline}
+        onChangeText={(newTask) => {
+          setTask(newTask);
+        }}
+        value={task}
+      />
+      <DateInput date={new Date(dueDate)} onChange={setDueDate} />
+      <CheckBox value={done} onValueChange={setDone} />
     </View>
   );
 }
